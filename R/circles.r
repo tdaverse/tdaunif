1e934f0ac942c210d9167cebd4eb2c88b88e82bc
corybrunson <jabrunso@vt.edu>
2019-01-29 16:04:40 -0500

    sinusoidal circle

--- R/circles.r
+++ R/circles.r
@@ -13,8 +13,8 @@ NULL
 
 #' @rdname circles
 #' @export
-sample_circle <- function(n, sd = 0) {
-  theta <- stats::runif(n = n, min = 0, max = 2 * pi)
+sample_circle_flat <- function(n, sd = 0) {
+  theta <- stats::runif(n = n, min = 0, max = 2*pi)
   res <- cbind(x = cos(theta), y = sin(theta))
   if (sd != 0) res <- res + rmvunorm(n = n, d = 2, sd = sd)
   res
@@ -22,10 +22,14 @@ sample_circle <- function(n, sd = 0) {
 
 #' @rdname circles
 #' @export
+sample_circle <- sample_circle_flat
+
+#' @rdname circles
+#' @export
 sample_circles_interlocked <- function(n, sd = 0) {
   theta <- stats::runif(n = n, min = 0, max = 4 * pi)
-  theta1 <- theta[theta < 2 * pi]
-  theta2 <- theta[theta >= 2 * pi]
+  theta1 <- theta[theta < 2*pi]
+  theta2 <- theta[theta >= 2*pi]
   res <- rbind(
     cbind(x = cos(theta1), y = sin(theta1), z = 0),
     cbind(x = cos(theta2) + 1, y = 0, z = sin(theta2))
@@ -33,5 +37,15 @@ sample_circles_interlocked <- function(n, sd = 0) {
   if (sd != 0) {
     res <- res + rmvunorm(n = length(theta1, theta2), d = 3, sd = sd)
   }
+  res
+}
+
+#' @rdname circles
+#' @export
+sample_circle_sinusoidal <- function(n, sd = 0) {
+  theta <- stats::runif(n = n, min = 0, max = 2*pi)
+  x <- cos(theta); y <- sin(theta)
+  res <- cbind(x = x, y = y, z = x*y)
+  if (sd != 0) res <- res + rmvunorm(n = n, d = 3, sd = sd)
   res
 }
