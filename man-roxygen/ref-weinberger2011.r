d2ceed53185810545c70c3c7bf4d57ca289a478d
Jason Cory Brunson <cornelioid@gmail.com>
2019-02-06 16:07:58 -0500

    README

---
+++ man-roxygen/ref-weinberger2011.r
@@ -0,0 +1,5 @@
+#' @references
+#'
+#' S Weinberger (2011) What is... Persistent Homology? _Notices of the American
+#' Mathematical Society_ **58**(01), 36--39.
+#' \url{https://www.ams.org/journals/notices/201101/rtx110100036p.pdf}
