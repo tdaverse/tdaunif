25cd16cf7f3cf2656b33e96a8010409fc8197fa0
20demkowiczbr <20demkowiczbr@fpsct.org>
2019-07-16 10:38:48 -0400

    Rejection sampler to uniformly sample an archimedean spiral, with documentation and examples.

---
+++ R/archimedean-spirals.r
@@ -0,0 +1,52 @@
+#' @title Sample from archimedean spirals
+#'
+#' @description These functions generate uniform samples from archimedean
+#'   spirals in 2-dimensional space.
+#'
+#' @details (Details.)
+
+#' @name arch-spirals
+#' @param n Number of observations.
+#' @param min_wrap The wrap of the spiral from which sampling begins
+#' @param max_wrap The wrap of the spiral where sampling ends
+#' @example inst/examples/ex-archimedean-spirals.r
+NULL
+
+#' @rdname arch-spirals
+#' @export
+sample_arch_spiral <- function(n, min_wrap = 0, max_wrap = 1){
+  theta <- rs_spiral(n, min_wrap, max_wrap)
+  #Applies modified theta values to parametric equation of spiral
+  res <- cbind(
+    x = (theta * cos(theta)),
+    y = (theta * sin(theta))
+  )
+}
+
+#Rejection sampler
+rs_spiral <- function(n, min_wrap, max_wrap){
+  #Sets the minimum and maximum values of theta according to number of spirals
+  #the user desired
+  min_theta <- min_wrap * 2*pi
+  max_theta <- max_wrap * 2*pi
+  x <- c()
+  #Keep looping until desired number of observations is achieved
+  while(length(x) < n){
+    theta <- runif(n, min_theta, max_theta)
+    jacobian <- jd_spiral()
+    #applies the Jacobian scalar value to each value of theta
+    jacobian_theta <- sapply(theta, jacobian)
+    #Density threshold is the greatest jacobian value in the spiral, and the
+    #area of least warping
+    density_threshold <- runif(n, 0, jacobian(max_theta))
+    #Takes theta values that exceed the density threshold, and throws out the
+    #rest
+    x <- c(x, theta[jacobian_theta > density_threshold])
+  }
+  x[1:n]
+}
+
+#Jacobian determinant of figure eight
+jd_spiral <- function(){
+  function(theta)theta
+}
---
+++ inst/examples/ex-archimedean-spirals.r
@@ -0,0 +1,3 @@
+#Uniformly sampled archimedean spiral in 2-space, with 2 wraps
+x <- sample_arch_spiral(720, min_wrap = 0, max_wrap = 2)
+plot(x, asp = 1, pch = 19, cex = .5)
