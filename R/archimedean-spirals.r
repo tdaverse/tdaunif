df821b1d2d49cfe0265f335834d714321523774c
20demkowiczbr <20demkowiczbr@fpsct.org>
2019-07-17 09:57:30 -0400

    Added two more examples of spiral function, auto-wrapped comments, and added details to the function.

--- R/archimedean-spirals.r
+++ R/archimedean-spirals.r
@@ -3,7 +3,14 @@
 #' @description These functions generate uniform samples from archimedean
 #'   spirals in 2-dimensional space.
 #'
-#' @details (Details.)
+#' @details The archimedean spiral starts at the origin and wraps around itself
+#'   such that distances between all the spiral branches are equivalent. The
+#'   specific parametrization was taken from (insert). The uniform sample is
+#'   generated through a rejection sampling process as described by Diaconis
+#'   (2013).
+
+#' @template ref-diaconis2013
+#' 
 
 #' @name arch-spirals
 #' @param n Number of observations.
@@ -25,16 +32,19 @@ sample_arch_spiral <- function(n, min_wrap = 0, max_wrap = 1){
 
 #Rejection sampler
 rs_spiral <- function(n, min_wrap, max_wrap){
-  #Sets the minimum and maximum values of theta according to number of spirals the user desired
+  #Sets the minimum and maximum values of theta according to number of spirals
+  #the user desired
   min_theta <- min_wrap * 2*pi
   max_theta <- max_wrap * 2*pi
   x <- c()
   #Keep looping until desired number of observations is achieved
   while(length(x) < n){
     theta <- runif(n, min_theta, max_theta)
-    #Density threshold is the greatest jacobian value in the spiral, and the area of least warping
+    #Density threshold is the greatest jacobian value in the spiral, and the
+    #area of least warping
     density_threshold <- runif(n, 0, max_theta)
-    #Takes theta values that exceed the density threshold, and throws out the rest
+    #Takes theta values that exceed the density threshold, and throws out the
+    #rest
     x <- c(x, theta[theta > density_threshold])
   }
   x[1:n]
--- inst/examples/ex-archimedean-spirals.r
+++ inst/examples/ex-archimedean-spirals.r
@@ -1,3 +1,12 @@
-#Uniformly sampled archimedean spiral in 2-space, with 2 wraps
-x <- sample_arch_spiral(720, min_wrap = 0, max_wrap = 2)
+#Uniformly sampled archimedean spiral in 2-space, with 1 wrap
+x <- sample_arch_spiral(360, min_wrap = 0, max_wrap = 1)
+plot(x, asp = 1, pch = 19, cex = .5)
+
+#Uniformly sampled archimedean spiral in 2-space, with 5 wraps
+x <- sample_arch_spiral(360, min_wrap = 0, max_wrap = 5)
+plot(x, asp = 1, pch = 19, cex = .5)
+
+#Uniformly sampled archimedean spiral in 2-space, with 5 wraps and starting from
+#2 wraps
+x <- sample_arch_spiral(360, min_wrap = 2, max_wrap = 5)
 plot(x, asp = 1, pch = 19, cex = .5)
