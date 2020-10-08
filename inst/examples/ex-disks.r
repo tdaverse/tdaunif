bbc891e45a120588e696c908ebffd6762d3bd5fb
corybrunson <cornelioid@gmail.com>
2020-10-08 12:36:51 -0400

    docs: proofread vignette + change 'disc' to 'disk'

---
+++ inst/examples/ex-disks.r
@@ -0,0 +1,7 @@
+# Uniformly sampled unit disk in 2-space
+x <- sample_disk(1800, sd = 0)
+plot(x, asp = 1, pch = 19, cex = .5)
+
+# Uniformly sampled unit disk in 2-space with Gaussian noise
+x <- sample_disk(1800, sd = .1)
+plot(x, asp = 1, pch = 19, cex = .5)
