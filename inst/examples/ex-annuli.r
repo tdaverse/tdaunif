509c6d2d00dee310b88d2e778a892584ff855b06
Jason Cory Brunson <cornelioid@gmail.com>
2024-01-27 14:24:29 -0500

    extend disk sampler to annulus sampler

--- inst/examples/ex-annuli.r
+++ inst/examples/ex-annuli.r
@@ -7,3 +7,7 @@ plot(x, asp = 1, pch = 19, cex = .5)
 # Uniformly sampled unit disk in 2-space with Gaussian noise
 x <- sample_disk(1800, sd = .1)
 plot(x, asp = 1, pch = 19, cex = .5)
+
+# Uniformly sampled annulus
+x <- sample_annulus(100, r = .4, sd = 0)
+plot(x, asp = 1, pch = 19, cex = .5)
